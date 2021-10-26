bec9f1573ae83c07b8d62700acc4ad8cd02c4ff7
manekajal <manekajal2014@gmail.com>
2021-10-26 15:01:24 +0530

    [Kajal]:DB Connection established

---
+++ src/main/java/Connection/DBConnector.java
@@ -0,0 +1,43 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+public class DBConnector {
+    private static Connection connection;
+
+    public static void main(String[] args) throws SQLException {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service?useSSl=false";
+        String userName = "root";
+        String password = "Kajal@95";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to database" + jdbcUrl);
+            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            System.out.println("Connection is successfull" + connection);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverlist = DriverManager.getDrivers();
+        while (driverlist.hasMoreElements()) {
+            Driver driver = driverlist.nextElement();
+            System.out.println(" " + driver.getClass().getName());
+
+        }
+    }
+}
